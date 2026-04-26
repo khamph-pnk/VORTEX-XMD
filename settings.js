f7d5c5ac1aa8f61500d50c5d49a0af78b908599a
Khamph <223732714+khamph-pnk@users.noreply.github.com>
2026-04-26 23:17:18 +0300

    Update settings.js - Change all references to KHAMPH-XMD

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const settings = {
 
 SESSION_ID: process.env.SESSION_ID || '',
   
-botName: process.env.BOT_NAME || "Hans_Tz",
+botName: process.env.BOT_NAME || "Khamph",
   
-botOwner: process.env.BOT_OWNER || 'Hans_Tz',
+botOwner: process.env.BOT_OWNER || 'Khamph',
 
 Botprefix: process.env.PREFIX || '.',
   
@@ -16,9 +16,9 @@ description: process.env.BOT_DESCRIPTION || "This is a bot for managing group co
   
 version: process.env.BOT_VERSION || "2.0.0",
   
-updateZipUrl: process.env.UPDATE_ZIP_URL || "https://github.com/HansTz/VORTEX-XMD/archive/refs/heads/main.zip",
+updateZipUrl: process.env.UPDATE_ZIP_URL || "https://github.com/khamph-pnk/KHAMPH-XMD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
 
-///MADE BY HANS_TZ
+///MADE BY KHAMPH
